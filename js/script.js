09f10cc17fb99b2e5df79046d59de84a32de6bfc
mahmud035 <mahamudulhasan4148@gmail.com>
2022-07-29 06:26:29 +0600

    hide navbar when clicked nav-link

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Nav Hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+
+navBar.forEach((a) => {
+  a.addEventListener('click', () => {
+    navCollapse.classList.remove('show');
+  });
+});
